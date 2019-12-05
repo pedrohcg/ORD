0676b65ea0d3765c0b4b7fa4198a714a8a0a3076
pedrohcg <52712904+pedrohcg@users.noreply.github.com>
2019-12-05 00:23:20 -0300

    Update main.cpp

--- Huffman1/main.cpp
+++ Huffman1/main.cpp
@@ -7,38 +7,15 @@ using namespace std;
 
 int main(){
     huffman *h = new huffman();
-//    ifstream file;
-//    unsigned int frequency[256] = {};
-//    int a;
-//
-//    file.open("teste.txt", ios::in);
-//    assert(file.is_open());
-//
-//    a = file.get();
-//
-//    while(!file.eof()){
-//        //Contador de frequencia de caractere
-//        frequency[a]++;
-//
-//        string m = h->writeByte(a);
-//        cout << m << endl;
-//
-//        char l = h->readByte(m);
-//        cout << l << endl;
-//
-//        cout << "frequencia" << endl;
-//        cout << frequency[a] << endl;
-//
-//        a = file.get();
-//    }
-
+    //Codification
     h->createQueue("teste3.txt");
     node *a;
-    a = h->createTree("teste3.txt");
+    a = h->createTree();
     h->createCodes(a, "");
     h->printTree(a);
     h->compress("teste3.txt");
-    h->decompress("result/teste3.txt.huffman", a);
+    //Decodification
+    h->decompress("result/teste3.txt.huffman");
     //file.close();
 
    return 0;
